2b9214a52b93d41b5cf4f432184c018ce06206f0
zagabond <jhj46456@gmail.com>
2021-05-19 19:53:50 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -48,6 +48,16 @@ echo -e "${BG_GREEN} Upgrading... ${NC}"
 
 sudo apt upgrade -y
 
+echo
+echo -e "${BG_GREEN} Updating... ${NC}"
+
+sudo apt update -y
+
+echo
+echo -e "${BG_GREEN} Upgrading... ${NC}"
+
+sudo apt upgrade -y
+
 # ---------------------
 # swap memory
 # ---------------------
